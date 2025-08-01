a14307b67423b47faa902a4f7df542ceec002b63
rzkypribadi <rzkyalamak@gmail.com>
2025-08-01 18:48:32 +0700

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,16 @@
+// Konfigurasi Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyANSYmNZHRuFE6p6lga3cEkYTCQU67xT7g",
+  authDomain: "database-invest.firebaseapp.com",
+  databaseURL: "https://database-invest-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "database-invest",
+  storageBucket: "database-invest.firebasestorage.app",
+  messagingSenderId: "465429517765",
+  appId: "1:465429517765:web:de893fc118784569ef67ac",
+  measurementId: "G-XW756RZPZJ"
+};
 
+// Inisialisasi Firebase
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const database = firebase.database();
